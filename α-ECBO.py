a2d8e36ad2f522063af877cd9e36061d31d416d7
Jellytrial <39354982+Jellytrial@users.noreply.github.com>
2018-10-31 16:51:04 +0900

    Update α-ECBO.py

--- α-ECBO.py
+++ α-ECBO.py
@@ -2,6 +2,7 @@ import numpy as np
 from pulp import *
 import math
 import matplotlib.pyplot as plt
+from sklearn.metrics.cluster import calinski_harabaz_score
 
 """program step:
    1. init membership U or centroids randomly
@@ -116,7 +117,7 @@ def upUwithSimplex(dataSet, centroids, c, A1, A2):
 
 def ECBO(dataSet, c):
     n, m = dataSet.shape
-    clusterAssment = np.mat(np.zeros((n, m)))
+    clusterAssment = np.mat(np.zeros((n, 1)))
 
     #1. init membership  and centroids
     #initail_U = initial_U(dataSet, c)
@@ -132,12 +133,19 @@ def ECBO(dataSet, c):
     centroids = calCentroids(dataSet, U, c)
 
     #while last_centroids.all() != centroids.all():
+    #count = 0
+    #while (count < 2):
     lastcentroids = centroids
     J, U = upUwithSimplex(dataSet, lastcentroids, c, A1, A2)
     centroids = calCentroids(dataSet, U, c)
+        #count += 1
 
     #assgin data to clusters
-    for i in range(n):
+    for k in range(n):
+        for i in range(c):
+            if (U[k, i] == 1):
+                clusterAssment[k] = i
+    """for i in range(n):
         minDist = 10000.0
         minIdex = 0
         for j in range(c):
@@ -146,12 +154,16 @@ def ECBO(dataSet, c):
                 minDist = distance #update min distance
                 minIdex = j #update assignment of data to cluster
         if clusterAssment[i, 0] != minIdex:
-            clusterAssment[i, :] = minIdex
+            clusterAssment[i, :] = minIdex"""
 
+    label_pred = clusterAssment
+    #print(label_pred)
+    CHI = calinski_harabaz_score(dataSet, label_pred)
     print('final U:\n', np.mat(U))
     print('final centroids:\n', centroids)
     print('objective function:', value(J.objective))
-
+    print('cluster assignment:', clusterAssment)
+    print('Calinski-Harabaz Index:', CHI)
     for i in range(c):
         print(np.sum(U[:, i]))
     return U, centroids, J, clusterAssment
@@ -162,29 +174,46 @@ def showCluster(dataSet, c, centroids, clusterAssment):
 
     # plot centroids
     for j in range(c):
-        plt.scatter(centroids[j, 0], centroids[j, 1], c='k', marker='D')
+        plt.scatter(centroids[j, 0], centroids[j, 1], c='k', marker='D', label='centroids')
 
     #polt all samples
     mark = ['o', 'o', 'o', 'o', '^', '+', 's', 'd', '<', 'p']
     color = ['r', 'b', 'g', 'm', 'c', 'y']
     for i in range(n):
-            markIndex = int(clusterAssment[i, 0])
-            #print(markIndex)
-            plt.scatter(dataSet[i, 0], dataSet[i, 1], marker=mark[markIndex], c=color[markIndex], alpha = 0.6)
-            #shape = np.sum(U[:, i])
-            #plt.legend((showData, center), ('cluster(%s)'%shape, 'centroids'), loc=4)
+        markIndex = int(clusterAssment[i])
+        #print(markIndex)
+        plt.scatter(dataSet[i, 0], dataSet[i, 1], marker=mark[markIndex], c=color[markIndex], alpha = 0.6)
+        #shape = np.sum(U[:, i])
+        #plt.legend((dataSet, centroids), ('cluster', 'centroids'))
+    """type1=[]
+    type2=[]
+    type3=[]
+    type4=[]
+    for i in range(n):
+        if clusterAssment[i, 0] == 0:
+            type1.append(dataSet[i])
+            n_1 = len(type1[0])
+        if clusterAssment[i, 0] == 1:
+            type2.append(dataSet[i])
+            n_2 = len(type2[0])
+        if clusterAssment[i, 0] == 2:
+            type3.append(dataSet[i])
+            n_3 = len(type3[0])
+        if clusterAssment[i, 0] == 3:
+            type4.append(dataSet[i])
+            n_4 = len(type4[0])
+    plt.legend((type1, type2, type3, type4), ('Cluster1(%s)'%n_1, 'Cluster2(%s)'%n_2, 'Cluster3(%s)'%n_3, 'Cluster4(%s)'%n_4))"""
 
     plt.show()
 
 if __name__ == '__main__':
-    dataSet = loadData('D:/Tsukuba/My Research/Program/dataset/1_normal_data/normal_data.csv')
-    c = 4  #cluster number
+    dataSet = loadData('D:/Tsukuba/My Research/Program/dataset/3_circles_with_diffR/circles_with_diffR.csv')
+    c = 2  #cluster number
     k = math.floor(len(dataSet) / c) #cluster size
-    alpha = 15 #margin
+    alpha = 28 #margin
     A1 = k - alpha
     A2 = k + alpha
     U, centroids, J, clusterAssment = ECBO(dataSet, c)
     showCluster(dataSet, c, centroids, clusterAssment)
-
 
 
